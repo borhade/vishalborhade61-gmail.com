ae3085f02e09d58f37703b502c21154900f02c18
borhade <vishalborhade61@gmail.com>
2020-03-27 10:31:18 +0530

    create edit form route

--- app/router/crud_router.js
+++ app/router/crud_router.js
@@ -1,22 +1,32 @@
 var express = require('express');
+var base_url ='E:/vishalborhade61-gmail.com-master/vishalborhade61-gmail.com/app/';
 var router = express.Router();
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'test_1'
-  });
-connection.connect();
-
+var dbconnection = require(base_url+'router/db_connection');
 router.get('/',function(req,res){
-  connection.query('SELECT * FROM worker',function(err,rows,fields){
+  dbconnection.query('SELECT * FROM worker',function(err,rows,fields){
      if (err) throw err
     res.render('index',{
           pagetitle:'User List',
           worker_data:rows
      });
   });
-connection.end();
 });
+
+//-----delete----------------------------//
+
+router.delete('/crud/:id',function(req,res){
+  var delete_id = req.params.id;
+  var delete_query = `DELETE FROM worker where WORKER_ID=${delete_id}`;
+    dbconnection.query(delete_query,function(err,rows,fields){
+      if(err) throw err;
+  });
+});
+
+// edit form
+router.get('/edit_record/:id',function(req,res){
+    res.render('edit_form',{
+        pagetitle:'User Edit Form',
+    });
+});
+
 module.exports= router;
