e31840fcd7fa194acfe4658058b28dc8d7fd9bf9
borhade <vishalborhade61@gmail.com>
2020-03-27 19:18:39 +0530

    insert module completed

--- app/public/custom.js
+++ app/public/custom.js
@@ -1,3 +1,17 @@
+$('#add_user_record').submit(function(e){
+   e.preventDefault();
+   $.ajax({
+        url:'add_record_to_db',
+        type:'POST',
+        data:$(this).serialize()
+   }).done(function(res){
+      console.log(res);
+   });
+
+});
+
+
+
 $(document).on('click','.delete_record',function(){
   var record_delete_id = $(this).attr('data-id');
   $.ajax({
